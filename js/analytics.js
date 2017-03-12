3e1e5f3a143ea6aa844770d8aecd9d745b4d24d1
Apurva Tripathi <apurvatripathi13@gmail.com>
2017-03-12 16:33:52 +0530

    added google analytics

---
+++ js/analytics.js
@@ -0,0 +1,11 @@
+// This is the analytics code for aptr13.me
+// Tracking ID - 'UA-93518798-1'
+// Setup By ApTr13
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-93518798-1', 'auto');
+ga('send', 'pageview');
